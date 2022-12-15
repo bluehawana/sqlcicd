f3dd28aa5477f7800e4e273178034095a135222c
Hongzhi Li <bluehawana@gmail.com>
2022-12-15 15:49:42 +0100

    some new changes regarding new contents

---
+++ Product.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NULL, 
+    [Cost] NUMERIC NULL
+)
---
+++ Table1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NULL, 
+    [Price] NUMERIC NULL, 
+    [Cost] NUMERIC NULL
+)
